818fea38441498e804819717004b1888823db616
Igor-Manko <manko161igor@gmail.com>
2022-08-08 12:57:02 +0700

    add SelectionSort

--- Program_Example/SelectionSort/Program.cs
+++ Program_Example/SelectionSort/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сортировка выбором
+
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите {i + 1} значение массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
+for (int i = 0; i < n - 1; i++)
+{
+    int MinIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if (array[j] < array[MinIndex])
+        {
+            MinIndex = j;
+        }
+    }
+    int temp = array[MinIndex];
+    array[MinIndex] = array[i];
+    array[i] = temp;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+
